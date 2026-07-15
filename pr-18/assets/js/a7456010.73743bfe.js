d9a8c0a0552ea33868e37ed4abbdecca257e50ad
GabrielFerreiraMendes <GabrielFerreiraMendes@users.noreply.github.com>
2026-07-15 19:53:43 +0000

    deploy: 543ee95d86117d6752780ab99d1ac9435280f7f0

---
+++ pr-18/assets/js/a7456010.73743bfe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkminusframework_docs=self.webpackChunkminusframework_docs||[]).push([["56"],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
